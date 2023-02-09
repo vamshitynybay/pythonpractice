f48e075d5cb2517f4b5ea528453cc6b6b6a456be
TYNYBAY 6 <vamshidher.ponnala@tynybay.com>
2023-02-09 17:02:32 +0530

    included '''

--- Biggest_3Numbers.py
+++ Biggest_3Numbers.py
@@ -1,5 +1,5 @@
 #Program to find the biggest of given 3 numbers from keyboard
-
+'''
 n1=int(input("Enter First Number:"))
 n2=int(input("Enter Second Number:"))
 n3=int(input("Enter Third Number:"))
@@ -10,7 +10,7 @@ elif n2>n3:
 else:
     print("Biggest number is:",n3)
 
-
+'''
 n1=eval(input("Enter First Number:"))
 n2=eval(input("Enter Second Number:"))
 n3=eval(input("Enter Third Number:"))
